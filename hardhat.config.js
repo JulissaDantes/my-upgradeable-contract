83c0e7c87eba075df1e314ddc1fff6c5cc12a379
JulissaDantes <julissadcj@gmail.com>
2022-10-10 10:21:03 -0500

    Add storage layout to compilation output

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,15 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-etherscan");
 require('@openzeppelin/hardhat-upgrades');
+const { internalTask } = require('hardhat/config');
+const { TASK_COMPILE_SOLIDITY_GET_COMPILER_INPUT } = require('hardhat/builtin-tasks/task-names');
 require("dotenv").config();
+
+internalTask(TASK_COMPILE_SOLIDITY_GET_COMPILER_INPUT, async (args, hre, runSuper) => {
+  const input = await runSuper();
+  input.settings.outputSelection['*']['*'].push('storageLayout');
+  return input;
+});
 
 module.exports = {
   solidity: "0.8.4",
